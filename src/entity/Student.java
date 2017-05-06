95d694c3fdbcb82cef6561dc7edb9db2f4f80304
lxd123qwe <lxdxyhd@163.com>
2017-05-06 23:19:04 +0800

    练习，第一次同步git，2017/5/6 11:14

---
+++ src/entity/Student.java
@@ -0,0 +1,64 @@
+package entity;
+
+public class Student {
+	private int id;
+	private String name;
+	private double score;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	public Student(int id, String name, double score) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.score = score;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", score=" + score + "]";
+	}
+	
+	
+
+}
---
+++ src/test/Test.java
@@ -0,0 +1,21 @@
+package test;
+
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("1");
+		//读取配置文件
+		Configuration cfg=new Configuration().configure();
+		System.out.println("2");
+		SchemaExport export=new SchemaExport(cfg);
+		//第一个true：是否显示sql语句，
+		//第二个true：原来有内容是否刷新
+		export.create(true, true);
+		
+			
+	}
+
+}
